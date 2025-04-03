d4e85523888761de3232cab09caec2af1c08f6e7
Leo0liveira <lfofreitas@gmail.com>
2025-04-03 00:33:12 -0300

    Changes:
    - Add botão surpresa
    - Correção musicas

--- FirstMonth/src/App.jsx
+++ FirstMonth/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import Header from './components/Header';
 import Gallery from './components/Gallery';
 import Letters from './components/Letters';
@@ -8,19 +8,18 @@ import './styles/App.css';
 
 import popupMusic from './assets/musica_popup.mp3';
 import backgroundMusic from './assets/musica_fundo.mp3';
+
 function App() {
   const [isPopupVisible, setIsPopupVisible] = useState(false);
   const popupAudioRef = useRef(null);
   const backgroundAudioRef = useRef(null);
 
-  useEffect(() => {
-    setTimeout(() => {
-      setIsPopupVisible(true);
-      if (popupAudioRef.current) {
-        popupAudioRef.current.play();
-      }
-    }, 500);
-  }, []);
+  const openPopup = () => {
+    setIsPopupVisible(true);
+    if (popupAudioRef.current) {
+      popupAudioRef.current.play().catch(err => console.log("Erro ao tocar áudio:", err));
+    }
+  };
 
   const closePopup = () => {
     setIsPopupVisible(false);
@@ -37,14 +36,19 @@ function App() {
 
   return (
     <div className="app-container">
-      <audio ref={popupAudioRef} src={popupMusic} loop />
+      <audio ref={popupAudioRef} src={popupMusic} />
       <audio ref={backgroundAudioRef} src={backgroundMusic} loop />
-      <Header />
+
+      {/* Passamos a função openPopup como propriedade */}
+      <Header openPopup={openPopup} />
+
       <main>
         <Letters />
         <Gallery />
       </main>
+
       <Popup isVisible={isPopupVisible} onClose={closePopup} />
+
       <Footer />
     </div>
   );
--- FirstMonth/src/components/Header.jsx
+++ FirstMonth/src/components/Header.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
-import '../styles/Header.css'
+import React from 'react';
+import '../styles/Header.css';
 import tituloImage from '../assets/text.png';
 
-function Header() {
+function Header({ openPopup }) {
   return (
     <header className="header">
-        <img src={tituloImage} alt="Meu Amorzinho" className="header-title-img" />
-       <nav className="header-nav">
-         <a href="#letters">Cartinhas</a>
-         <a href="#gallery">Fotos</a>
-       </nav>
+      <img src={tituloImage} alt="Meu Amorzinho" className="header-title-img" />
+      <nav className="header-nav">
+        <a href="#letters">Cartinhas</a>
+        <button className="popup-button" onClick={openPopup}>
+          🚨 Clique por sua conta e risco 🚨
+        </button>
+        <a href="#gallery">Fotos</a>
+      </nav>
     </header>
-  )
+  );
 }
 
 export default Header;
